7ee8221e4260865197a8a66dfb5d299cde5d8ed1
Luciano Marani <lmarani@santafe.gov.ar>
2023-02-10 14:07:26 -0300

    Ex 1.6

---
+++ unicafe/src/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Title = (props) => {
+  return <h1>{props.text}</h1>;
+};
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
+
+const Stat = (props) => {
+  return (
+    <div>
+      {props.text} {props.value}
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => {
+    setGood(good + 1);
+  };
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const increaseBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <Title text="give feedback" />
+      <Button onClick={increaseGood} text="good" />
+      <Button onClick={increaseNeutral} text="neutral" />
+      <Button onClick={increaseBad} text="bad" />
+      <Title text="statistics" />
+      <Stat text="good" value={good} />
+      <Stat text="neutral" value={neutral} />
+      <Stat text="bad" value={bad} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
